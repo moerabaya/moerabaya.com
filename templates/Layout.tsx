c3368467ee8da58e813877d3895d535d6e58294c
Mohammed Rabaya <rabaya.moe@gmail.com>
2022-09-10 23:37:53 +0300

    Adding theme to styled components

--- templates/Layout.tsx
+++ templates/Layout.tsx
@@ -1,19 +1,23 @@
 import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
 import Footer from '../components/Footer';
 import MenuList from '../components/Menu';
+import { theme } from 'styles/theme';
 
 export default function Layout({ children, pageProps }:any) {
   return (
-    <div className='content'>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
-        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
+    <ThemeProvider theme={theme}>
+      <div className='content'>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+          <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
 
-      </Head>
-      <MenuList  {...pageProps} />
-      <main>{children}</main>
-			<Footer />
-    </div>
+        </Head>
+        <MenuList  {...pageProps} />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
