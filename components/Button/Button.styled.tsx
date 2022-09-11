21967489515d330f3a7808240ba2f1381773c845
Mohammed Rabaya <rabaya.moe@gmail.com>
2022-09-11 22:46:30 +0300

    Update Button.styled.tsx

--- components/Button/Button.styled.tsx
+++ components/Button/Button.styled.tsx
@@ -1,10 +1,11 @@
-import styled, { ButtonInterface } from "styled-components";
+import styled, { ButtonComponent } from "styled-components";
 import { Base } from "../Base";
 
-export const Button = styled(Base)<ButtonInterface>`
+export const Button = styled((props) => <Base {...props} as="button" />)<ButtonComponent>`
 	appearance: none;
 	border: none;
 	outline: none;
+	color: var(--text-color);
 	text-decoration: none;
 	border-radius: 0.25em;
 	padding: 0.3em 0.4em;
@@ -12,9 +13,9 @@ export const Button = styled(Base)<ButtonInterface>`
 	font-size: ${props => props.size == "medium" ? "1.25em" : props.size == "large" ? "1.5em" : "1em"};
 	background-color: transparent;
 	&:hover {
-		background-color: var(--border-color);
+		background-color: rgba(var(--text-color-rgb), 0.1);
 	}
 	&:active {
-		background-color: var(--border-color);
+		background-color: rgba(var(--text-color-rgb), 0.1);
 	}
 `;
