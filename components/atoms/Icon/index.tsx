97f6851ee0e313cf5b5f2ead2ec75134ff0adfca
Mohammed Rabay'a <rabaya.moe@gmail.com>
2022-09-23 18:06:11 +0300

    Icon

---
+++ components/atoms/Icon/index.tsx
@@ -0,0 +1,23 @@
+import styled, { css, IconProps } from "styled-components";
+import _ from "lodash";
+
+const Icon = styled.span<IconProps>`
+	display: inline-block;
+	font-size: ${props => !_.isString(props.size) ? `${props.size}px` : props.size};
+	vertical-align: middle;
+	${props => {
+		switch(props.slot) {
+			case "end":
+				return css`
+					margin-inline-end: 0.5em;
+				`;
+				break;
+			default:
+				return css`
+					margin-inline-start: 0.5em;
+				`;
+		}
+	}}
+`;
+
+export default Icon;
