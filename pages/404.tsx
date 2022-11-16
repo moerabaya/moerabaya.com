c330b58bdfc93ffc41b71242e3ee65156869de35
Mohammed Rabay'a <rabaya.moe@gmail.com>
2022-11-16 08:49:47 +0000

    Add translated 404 Title

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,20 @@
+import useGlobalization from "hooks/useGlobalization";
+import Head from "next/head";
 import React from "react";
 
-const NotFound = () => (
-  <div className="page-content coming-soon-container">
-    <h1 className="notfound-title">
-      404 - Oh no&apos;s! We couldn&apos;t find that page
-    </h1>
-  </div>
-);
+const NotFound = () => {
+  const { getLocalizedString } = useGlobalization();
+  return (
+    <>
+      <Head>
+        <title>{getLocalizedString("404", "title")}</title>
+      </Head>
+      <div className="page-content coming-soon-container">
+        <h1 className="notfound-title">{getLocalizedString("404", "title")}</h1>
+      </div>
+    </>
+  );
+};
 NotFound.displayName = "NotFound";
 
 export default NotFound;
