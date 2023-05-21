2e05f022ec3761f6134eb84009fbe0bc83b045ed
Mohammed Rabay'a <rabaya.moe@gmail.com>
2023-05-22 00:18:20 +0200

    Update about dark mode

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
+import Image from "next/image";
 import { NextPage } from "next/types";
-import React from "react";
 import AnimatedView from "../components/atoms/AnimatedView";
 
 const About: NextPage<any> = () => {
@@ -43,33 +43,68 @@ const About: NextPage<any> = () => {
           content={require("../assets/images/metaimage.png")}
         />
       </Head>
-      <div className="container">
-        <br />
-        <AnimatedView>
-          <h2>
-            Hi, I&apos;m <strong>Mohammed Rabay&apos;a</strong>.
-          </h2>
-        </AnimatedView>
-        <br />
-        <AnimatedView delay={0.75}>
-          <h2>
-            A product-focused software engineer with over{" "}
-            {new Date().getFullYear() - 2015}+ years of experience working in
-            the field of UX and traditional development.
-          </h2>
-        </AnimatedView>
+      <div className="container mx-auto max-w-3xl text-lg flex my-16">
+        <div className="text-2xl">
+          <Image
+            src={
+              "https://en.gravatar.com/userimage/201100235/e812a2bff97470caf6299b1a96e5cc1e.png?size=1000"
+            }
+            alt="Portrait of Mohammed Rabay'a"
+            placeholder="blur"
+            blurDataURL='"https://en.gravatar.com/userimage/201100235/e812a2bff97470caf6299b1a96e5cc1e.png?size=1"'
+            width={400}
+            height={70}
+            className="overflow-hidden rounded-3xl"
+          />
+        </div>
+        <div className="ps-8">
+          <AnimatedView>
+            <h4 className="font-semibold text-xl text-stone-800 dark:text-neutral-50">
+              <span>Hola - </span>
+              <span>مرحبا</span>
+            </h4>
+          </AnimatedView>
+          <AnimatedView delay={0.75}>
+            <h4 className="text-lg text-stone-800 dark:text-neutral-50">
+              My name is Mohammed Rabay'a - A product-focused software engineer
+              currently based in{" "}
+              <strong
+                className="font-medium text-red-600"
+                style={{
+                  background:
+                    "linear-gradient(217deg, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047)",
+                  "-webkit-background-clip": "text",
+                  "-webkit-text-fill-color": "transparent",
+                }}
+              >
+                Amsterdam
+              </strong>
+              .
+            </h4>
+          </AnimatedView>
 
-        <br />
-        <br />
-        <AnimatedView delay={1.5}>
-          <h2>
-            Based in Amman, Jordan and currently designing and creating products
-            at{" "}
-            <a target="_blank" href="https://cura.healthcare/" rel="noreferrer">
-              <strong>Cura Healthcare</strong>
-            </a>
-          </h2>
-        </AnimatedView>
+          <AnimatedView delay={1.5}>
+            <h4 className="text-lg text-stone-800 dark:text-neutral-50">
+              With over 7 years of experience in designing and build softwares.
+              Currently developing products at{" "}
+              <a
+                target="_blank"
+                href="https://getrodeo.io/"
+                className="text-violet-600 hover:underline"
+                style={{
+                  background:
+                    "linear-gradient(217deg, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047)",
+                  "-webkit-background-clip": "text",
+                  "-webkit-text-fill-color": "transparent",
+                }}
+                rel="noreferrer"
+              >
+                <strong>Rodeo Software</strong>
+              </a>
+              .
+            </h4>
+          </AnimatedView>
+        </div>
       </div>
     </div>
   );
