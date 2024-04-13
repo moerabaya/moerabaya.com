3b938265d4c8d9645d30023e0735a99468d25860
Mohammed Rabay'a <rabaya.moe@gmail.com>
2024-04-13 22:09:53 +0200

    feat: Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,28 @@ import Cookies from "universal-cookie";
 import consts from "consts";
 import ThemeProvider from "templates/ThemeProvider";
 import { getProjects } from "pages";
+import Script from "next/script";
 // prevent TypeScript errors on the css prop on arbitrary elements
 // import {} from 'styled-components/cssprop'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider {...pageProps}>
+      <Script
+        async
+        onError={(e: Error) => {
+          console.error("Script failed to load", e);
+        }}
+        src="https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
+        `}
+      </Script>
       <Layout pageProps={pageProps}>
         <Component {...pageProps} />
       </Layout>
